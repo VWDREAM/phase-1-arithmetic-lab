f809a1a5051012fd51b252a3a88320ed471729c0
VWDREAM <wanjiruvirginiah26@gmail.com>
2025-03-07 15:00:24 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
 // Write your code here
+const num1 =31
+const num2 =2
+const multiply = num1*num2
+console.log(multiply);
+
+const random = Math.floor(Math.random() * 50) + 1;
+
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4;
+
+
+console.log(mod);
+
+const numbers = new Set([5, 12, 20, 3, 7]);
+const maxNumber = Math.max(...Array.from(numbers));
+const max = 20;
+ console.log(max);
